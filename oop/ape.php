471dc2ab0266f776769ddbc7f431526b2a3f86cf
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-07-30 23:41:48 +0800

    Add files via upload

---
+++ oop/ape.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "animal.php";
+
+		$sungokong = new Ape("kera sakti");
+		$sungokong->get_yell() ;
+		
+	
+?>
---
+++ oop/frog.php
@@ -0,0 +1,9 @@
+<?php
+	
+	require "animal.php";
+
+		$kodok = new Frog("buduk");
+		$kodok->get_jump() ;
+		
+
+?>
