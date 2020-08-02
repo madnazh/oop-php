07866af53bd372574942e2ade355b63c30cd1226
Achmad Naufal A <68943252+madnazh@users.noreply.github.com>
2020-08-02 14:09:14 +0800

    Update animal.php

--- oop/animal.php
+++ oop/animal.php
@@ -1,74 +1,24 @@
 <?php
 
+	class animal{	
+		protected $name;
+		public $legs = 2;
+		public $cold_blooded = false;
 
-	class animal
-	{	
-		public $name;
-		public $legs;
-		public $cold_blooded;
-		public $yell;
-		public $jump;
-
-		public function __construct($name, $legs, $cold_blooded, $yell, $jump){
+		public function __construct($name){
 			$this->name = $name;
-			$this->legs = $legs;
-			$this->cold_blooded = $cold_blooded;
-			$this->yell = $yell
-			$this->jump = $jump
 
 		}
-		
 
-		public function set_Name($name){
-			$this->name = $name;
-			
-		}
-		public function get_Name(){
+
+		public function __get($name){
+			if(property_exists($this, $name)){
 			return $this->name;
-			
-		}
-
-
-		public function set_Legs($legs){
-			 $this->legs = $legs;
-		}	
-
-		public function get_Legs(){
-			 return $this->legs;
-		}
-
-
-		public function set_cold_blooded(){
-			 $this->cold_blooded = $cold_blooded;
-		}	
-
-		public function get_cold_blooded(){
-			 return $this->cold_blooded;
+			}
 		}
 
 		
 	}
-
-	class animal_ape extends animal
-	{	
-
-
-		public function get_yell() {
-			echo "Auooo";
-			}
-	}
-
-
-	class animal_frog extends animal
-	{	
-
-
-		public function get_jump(){
-			echo "hop hop";
-			}
-
-	}
-
 
 
 ?> 
